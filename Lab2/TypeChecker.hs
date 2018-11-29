be75c72a4bce51c40d91743acdf0107ed97e7c03
Lucas Bäckvall <lucasba@student.chalmers.se>
2018-11-29 11:11:14 +0100

    progress lab2

--- Lab2/TypeChecker.hs
+++ Lab2/TypeChecker.hs
@@ -9,9 +9,30 @@ import CPP.Abs
 import CPP.Print
 import CPP.ErrM
 
+type Env = (Sig, [Context])			-- functions and context stack
+type Sig = Map Id ([Type], Type)	-- function type signature
+type Context = Map Id Type			-- variables with their types
 
 typecheck :: Program -> Err ()
 typecheck p = return ()
+
+lookVar :: Env -> Id -> Err Type
+lookVar (_, []) id = fail $ "lookVar: Id not in Env"
+lookVar (sig, ((Map cid ctyp):cs)) id
+	| cid == id = return ctyp
+	| otherwise = lookVar (sig, cs) id
+
+updateVar :: Env -> Id -> Type -> Err Env
+updateVar (sig, cs) id typ = (sig, (updateVar' cs id typ))
+
+updateVar' :: [Context] -> Id -> Type -> [Context]
+updateVar' [] id typ = [(Map id typ)]
+updateVar' (c:cs) id typ = case c of 
+	(Map id typ)	-> (c:cs)
+	(Map id _)		-> fail $ "updateVar. Id has a different Type"
+	_				-> [c]++(updateVar' cs id typ)
+
+emptyEnv :: Env
 
 checkExp :: Env -> Type -> Exp -> Err ()
 checkExp env typ exp = do
@@ -22,15 +43,18 @@ checkExp env typ exp = do
 		fail $ "type of " ++ printTree exp
 
 inferExp :: Env -> Exp -> Err Type
-inferExp env exp = case x of
+inferExp env x = case x of
 	ETrue 	-> return Type_bool
 	EFalse	-> return Type_bool
 	
 	EInt a		-> return Type_int
 	EDouble a	-> return Type_double
 	
-	EId id		-> lookVar env id
---	EApp id exps	-> ples help
+	EId id			-> lookVar env id
+	EApp id exps	-> lookvar env id
+--	EApp id exps	-> do
+--		map (\exp -> inferExp env exp) exps
+--		lookVar env id
 	
 	EPostIncr id	-> lookVar env id
 	EPostDecr id	-> lookVar env id
@@ -54,6 +78,7 @@ inferExp env exp = case x of
 	
 	EAss id exp ->	lookVar env id
 
+-- inferArithmBin i boken.
 inferArithm :: Env -> Exp -> Exp -> Err Type
 inferArithm env a b = do
 	typ <- inferExp env a
@@ -62,7 +87,6 @@ inferArithm env a b = do
 	else
 		fail $ "type of expression " ++ printTree exp
 
--- Det blev 120% förvirring precis innan vi lämnade detta.
 checkStm :: Env -> Type -> Exp -> Err Env
 checkStm env val x = case x of
 	SExp exp -> do
@@ -70,13 +94,19 @@ checkStm env val x = case x of
 		return env
 	SDecls typ ids ->
 		map (\id -> updateVar env id typ) ids
-	SInit typ id exp -> do
-		if (checkExp env exp == typ) then
-			updateVar env id typ
-		else
-			fail $ "type of expression " ++ printTree exp
-	SReturn exp ->
+	SInit typ id exp ->
+		updateVar env id typ
+	SReturn exp -> do
+		inferExp env exp
 		return env
+	SWhile exp stm -> do
+		checkExp env Type_bool exp
+		checkStm env val stm
+	SBlock stms ->
+		map (\stm -> checkStm env val stm) stms
+	SIfElse exp stm0 stm1 -> do
+		checkExp env Type_bool exp
+		checkStm env val stm0
+		checkStm env val stm1
 
-updateVar :: Env -> Id -> Type -> Err ([Type], Type)
 
