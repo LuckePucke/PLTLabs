71365fc1de4666dc6217ac31e483bd533f1a5507
tove <svensson.tove@outlook.com>
2018-12-13 10:54:17 +0100

    stole more stuff

--- Lab3/Compiler.hs
+++ Lab3/Compiler.hs
@@ -29,6 +29,26 @@ type Output = [String]
 
 type Compile = RWS Env Output St
 
+type Addr = Int
+
+data Code
+  = Store Type Addr  -- ^ Store stack content of type @Type@ in local variable @Addr@.
+  | Load  Type Addr  -- ^ Push stack content of type @Type@ from local variable @Addr@.
+
+  | IConst Integer   -- ^ Put integer constant on the stack.
+  | Pop Type         -- ^ Pop something of type @Type@ from the stack.
+  | Return Type      -- ^ Return from method of type @Type@.
+  | Add Type         -- ^ Add 2 top values of stack.
+
+  | Call Fun         -- ^ Call function.
+
+  | Label Label      -- ^ Define label.
+  | Goto Label       -- ^ Jump to label.
+  | IfZ Label        -- ^ If top of stack is 0, jump to label.
+  | IfLt Type Label  -- ^ If prev <  top, jump.
+  
+  deriving (Show)
+
 builtin = [
 		((Id "printInt"), (Fun (Id "Runtime/printInt") (VVoid [VInt]))),
 		((Id "readInt"), (Fun (Id "Runtime/readInt") (VInt [])))
@@ -77,6 +97,13 @@ compile' str prg@(PDefs defs) = do
 	]
 
 compileFun :: lol -> String
+
+-- | Print a single instruction.  Also update stack limits
+-- om vi ska updatera stacken, måste vi inte ta in den i nån form av input då? 
+emit :: Code -> Compile ()
+emit = error $ "TODO: emit"
+
+
 
 defToIdFun :: Def -> (Id, Fun)
 defToIdFun (DFun typ id args stms) = Fun ((Id id), (FunType (typ, (map (\(ADecl atyp aid) -> atyp) args))))
