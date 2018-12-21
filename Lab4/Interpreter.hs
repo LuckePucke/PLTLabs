899e7bc93c278521c383312fbeae3baeeea4b734
Lucas Bäckvall <lucasba@student.chalmers.se>
2018-12-21 13:27:35 +0100

    Added code skeleton

--- Lab4/Interpreter.hs
+++ Lab4/Interpreter.hs
@@ -18,6 +18,7 @@ import qualified Data.Map as Map
 
 import Hgrammar.Abs
 import Hgrammar.Print
+import Hgrammar.ErrM
 
 -- | Evaluation strategy.
 
@@ -43,10 +44,6 @@ data Cxt = Cxt
 	, cxtEnv :: Env
 	}
 
--- | Error monad.
-
-type Err = Except String
-
 -- | Entry point: Program computes a number.
 
 interpret :: Strategy -> Program -> Err Integer
@@ -56,19 +53,38 @@ interpret strategy (Prog defs (DMain mainExp)) = do
 	val <- eval cxt mainExp
 	case val of
 		(VInt i) -> return i
-		otherwise -> Bad "main should return an integer."
+		otherwise -> fail "main should return an integer."
 
 eval :: Cxt -> Exp -> Err Value
 eval cxt exp = case exp of
-	EVar id	-> do
-		env <- cxtEnv cxt
-		case (Map.lookup id env) of
-			Just entry	-> return entry
-			Nothing		-> Bad "entry not in env"
+	EVar id	-> case (Map.lookup id (cxtEnv cxt)) of
+		Just entry	-> evalEntry cxt entry
+		Nothing		-> fail "entry not in env"
 
 	EInt i	-> return $ VInt i
-	EApp e1 e2 -> Bad "sluta" 
+
+	EApp e1 e2 -> fail "TODO: EApp e1 e2"
+	-- eval e2
+	-- smäll in någe (e2?) i cxt
+	-- return $ eval e1
+
+	EAdd e1 e2 -> fail "TODO"
+	ESub e1 e2 -> fail "TODO"
+	ELt e1 e2 -> fail "TODO"
+	EIf cond te fe -> fail "TODO"
+	EAbs id e -> fail "TODO"
+
+		
+
+evalEntry :: Cxt -> Entry -> Err Value
+evalEntry cxt (Val v) = return v
+evalEntry cxt (Clos e env) = eval (cxt { cxtEnv = env }) e
 
 newDefs :: Sig -> [Def] -> Sig
 newDefs sig [] = sig
-newDefs sig ((DDef id ids exp):ds) = newDefs (Map.insert id (FunDef ids exp)) ds
+newDefs sig ((DDef id ids exp):ds) = newDefs (Map.insert id (FunDef ids exp) sig) ds
+
+
+
+
+
