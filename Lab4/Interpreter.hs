899688d7264cff2ed2748b2ee25a08b78e39281a
Lucas Bäckvall <lucasba@student.chalmers.se>
2018-12-21 12:15:56 +0100

    tbd

--- Lab4/Interpreter.hs
+++ Lab4/Interpreter.hs
@@ -12,6 +12,7 @@ import Control.Monad.State
 import Control.Monad.Except
 
 import Data.Functor
+import Data.Maybe
 import Data.Map (Map)
 import qualified Data.Map as Map
 
@@ -24,6 +25,24 @@ data Strategy
   = CallByName
   | CallByValue
 
+-- | Context
+
+type Sig = Map Ident FunDef	-- function type signature
+data FunDef = FunDef [Ident] Exp
+type Env = Map Ident Entry
+data Entry
+	= Val Value
+	| Clos Exp Env
+data Value
+	= VInt Integer
+	| VClos Exp Env
+
+data Cxt = Cxt
+	{ cxtStrategy :: Strategy
+	, cxtSig :: Sig
+	, cxtEnv :: Env
+	}
+
 -- | Error monad.
 
 type Err = Except String
@@ -32,4 +51,24 @@ type Err = Except String
 
 interpret :: Strategy -> Program -> Err Integer
 interpret strategy (Prog defs (DMain mainExp)) = do
-  throwError $ "TODO: implement interpreter"
+	let sig = newDefs Map.empty defs
+	let cxt = Cxt strategy sig Map.empty
+	val <- eval cxt mainExp
+	case val of
+		(VInt i) -> return i
+		otherwise -> Bad "main should return an integer."
+
+eval :: Cxt -> Exp -> Err Value
+eval cxt exp = case exp of
+	EVar id	-> do
+		env <- cxtEnv cxt
+		case (Map.lookup id env) of
+			Just entry	-> return entry
+			Nothing		-> Bad "entry not in env"
+
+	EInt i	-> return $ VInt i
+	EApp e1 e2 -> Bad "sluta" 
+
+newDefs :: Sig -> [Def] -> Sig
+newDefs sig [] = sig
+newDefs sig ((DDef id ids exp):ds) = newDefs (Map.insert id (FunDef ids exp)) ds
